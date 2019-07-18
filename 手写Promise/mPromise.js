8c223afc4f4a4733ebe32aa5ef538e5bc9baaeb2
zhangshichun <zhangshichun@360.net>
2019-07-18 19:25:06 +0800

    增加race方法

--- 手写Promise/mPromise.js
+++ 手写Promise/mPromise.js
@@ -50,6 +50,11 @@ module.exports = class mPromise {
     this.state = PENDING
     fn(this._resolve, this._reject)
   }
+
+  /**
+   * @param {any} value
+   * @returns {mPromise}
+   */
   static resolve(value) {
     if (value instanceof mPromise) {
       return mPromise
@@ -58,6 +63,11 @@ module.exports = class mPromise {
       resolve(value)
     })
   }
+
+  /**
+   * @param {any} value
+   * @returns {mPromise}
+   */
   static reject(value) {
     if (value instanceof mPromise) {
       return mPromise
@@ -66,6 +76,7 @@ module.exports = class mPromise {
       reject(value)
     })
   }
+
   /**
    * @param {Iterator} promiseArr
    * @returns {mPromise}
@@ -96,6 +107,36 @@ module.exports = class mPromise {
             if (count == totle) {
               resolve(resArr)
             }
+          })
+          .catch(err => {
+            reject(err)
+          })
+      })
+    })
+  }
+
+  /**
+   * @param {Iterator} promiseArr
+   * @returns {mPromise}
+   * @description
+   * 1. 首先解释下入参，大多数情况下Promise.all的入参是一个数组，数组的每个元素都是Promise实例，
+   * 但实际上入参允许是具备Iterator接口的对象，但返回的每个成员都必须是Promise实例;(如果不是promise,则转换成promise)
+   * 2. 其原则为， Iterator接口的每个成员， 第一个状态都为fulfilled时， 返回的Promise实例变为fulfilled状态，且
+   * resolve方法入参为一个数组，数组的元素为第一个Promise实例的返回值。
+   * 3. 方法返回的Promise实例的.catch方法， 会抛出Iterator接口中， 第一个变为rejected状态的异常值。
+   */
+  static race(promiseArr) {
+    const mPromiseArr = Array.from(promiseArr).map(item => {
+      if (item instanceof mPromise) {
+        return item
+      }
+      return mPromise.resolve(item)
+    })
+    return new mPromise((resolve, reject) => {
+      mPromiseArr.forEach(item => {
+        item
+          .then(res => {
+            resolve(res)
           })
           .catch(err => {
             reject(err)
